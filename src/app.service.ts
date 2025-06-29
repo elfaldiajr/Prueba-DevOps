9554d29378b20a22c3fedc779076e9b61c79941e
Diego Martinez <diego.martinez01@alumnos.ucn.cl>
2025-06-29 17:21:09 -0400

    cambio a WARZONE

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHolaMundo(): string {
-    return 'Hola Mundo ETOILE!';
+    return 'Hola Mundo WARZONE!';
   }
 }
