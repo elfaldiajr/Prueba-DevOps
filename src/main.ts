5b9d3a9bd7cb02c95497606427cf154f29bb4695
Diego Martinez <diego.martinez01@alumnos.ucn.cl>
2025-06-29 18:45:28 -0400

    cambio web.config y package.json

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const port = process.env.PORT || 8000;
+  const port = process.env.PORT || 3000;
   await app.listen(port);
-  console.log(
-    `La aplicacion esta corriendo en el puerto: http://localhost:${port}`,
-  );
 }
 bootstrap();
